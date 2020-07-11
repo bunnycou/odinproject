0767a97953cd5e8d61a914cca5808d3ba336ce6a
Noah Cou <noahjcou@gmail.com>
2020-07-11 14:52:54 -0400

    finished some js exercises

--- webdev101/javascript-exercises-master/calculator/calculator.js
+++ webdev101/javascript-exercises-master/calculator/calculator.js
@@ -1,25 +1,43 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (x) {
+	var a = 0;
+	x.forEach(summ);
+	function summ(x) {
+		a += x;
+	}
+	return a;
 }
 
-function multiply () {
-	
+function multiply (x) {
+	var a = 1;
+	x.forEach(multi);
+	function multi(x) {
+		a *= x;
+	}
+	return a;
 }
 
-function power() {
-	
+function power(x, y) {
+	return Math.pow(x, y);
 }
 
-function factorial() {
+function factorial(x) {
+	var a = x;
+	if (a == 0) {
+		a = 1;
+	}
+	for (var i = x-1; i > 1; i--) {
+		a *= i;
+	}
 	
+	return a;
 }
 
 module.exports = {
--- webdev101/javascript-exercises-master/repeatString/repeatString.js
+++ webdev101/javascript-exercises-master/repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(one, num) {
+    var two = "";
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (var i = 0; i < num; i++) {
+            two += one;
+        }
+        return two;
+    }
 }
 
 module.exports = repeatString
