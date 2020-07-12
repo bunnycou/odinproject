93c9bc7a4b8c202193c7f07973d27fa15490d7f6
Noah Cou <noahjcou@gmail.com>
2020-07-11 21:51:21 -0400

    finished js exercises

--- webdev101/javascript-exercises-master/removeFromArray/removeFromArray.js
+++ webdev101/javascript-exercises-master/removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for (var i = 1; i < arguments.length; i++) {
+        arr = arr.filter(x => {
+            return x !== arguments[i];
+        })
+    }
 
+    return arr;
 }
 
 module.exports = removeFromArray
