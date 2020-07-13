bbf9ca53887327674d4e990a8ea43ae8163b16ec
Noah Cou <noahjcou@gmail.com>
2020-07-12 22:41:41 -0400

    Updated rps

--- webdev101/rock-paper-scissors-button/main.js
+++ webdev101/rock-paper-scissors-button/main.js
@@ -1,28 +1,31 @@
 // declare variables
-let choices = ["rock", "paper", "scissors"]
-let playerChoice = ""
-let computerChoice = ""
-let playerScore = 0
-let computerScore = 0
+let choices = ["rock", "paper", "scissors"];
+let playerChoice = "";
+let computerChoice = "";
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0;
 
 //get computer choice
 function computerPlay() {
-    return choices[Math.floor(Math.random() * choices.length)]
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 //run a round, calls all other functions
 function playRound(pc) {
-    playerChoice = pc
-    computerChoice = computerPlay()
+    playerChoice = pc;
+    computerChoice = computerPlay();
     console.log(computerChoice);
+    document.getElementById('throws').textContent = `Player: ${playerChoice} | Computer: ${computerChoice}`;
     if (((playerChoice == "rock") && (computerChoice == "scissors")) || ((playerChoice == "paper") && (computerChoice == "rock")) || ((playerChoice == "scissors") && (computerChoice == "paper"))) {
-        playerScore += 1
-        document.getElementById('player').textContent = `Player: ${playerScore}`
+        playerScore += 1;
+        document.getElementById('player').textContent = `Player: ${playerScore}`;
     } else if (((computerChoice == "rock") && (playerChoice == "scissors")) || ((computerChoice == "paper") && (playerChoice == "rock")) || ((computerChoice == "scissors") && (playerChoice == "paper"))) {
-        computerScore += 1
-        var csText = document.getElementById('computer').textContent = `Computer: ${computerScore}`
+        computerScore += 1;
+        var csText = document.getElementById('computer').textContent = `Computer: ${computerScore}`;
     } else {
-        return;
+        ties += 1;
+        document.getElementById('ties').textContent = `Ties: ${ties}`;
     }
 }
 
